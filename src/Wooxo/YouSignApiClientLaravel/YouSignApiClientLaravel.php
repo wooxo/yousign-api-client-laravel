c0bd08f0920b74a0a5f994b78141c56afa3f1f8e
Louis Laszlo <lflaszlo@wooxo.fr>
2015-09-09 15:36:57 +0200

    Fix

--- src/Wooxo/YouSignApiClientLaravel/YouSignApiClientLaravel.php
+++ src/Wooxo/YouSignApiClientLaravel/YouSignApiClientLaravel.php
@@ -8,13 +8,13 @@ use Illuminate\Support\Facades\Config;
  *
  * @package Wooxo\YouSignApiClientLaravel
  */
-class YouSignApiClientLaravel {
+class YouSignApiClientLaravel extends YsApi {
 
     /**
      * Constructor
      */
     public function __construct(){
-        $yousign = new YsApi();
+        $yousign = parent::__construct(null);
         $yousign->setLogin(Config::get('yousign-api-client-laravel::config.login'));
         if(!Config::get('yousign-api-client-laravel::config.isEncryptedPassword')) {
             $yousign->setPassword($yousign->encryptPassword(Config::get('yousign-api-client-laravel::config.password')));
@@ -24,7 +24,6 @@ class YouSignApiClientLaravel {
 
         $yousign->setApiKey(Config::get('yousign-api-client-laravel::config.api_key'));
         $yousign->setEnvironment(Config::get('yousign-api-client-laravel::config.environment'));
-        return $yousign;
     }
 
 }
